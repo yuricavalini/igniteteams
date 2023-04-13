7ed9211f9d74ea7f7849640cf6d2155766102a7d
Yuri Cavalini <yuricavalini@gmail.com>
2023-04-13 17:59:44 -0300

    feat(igniteteams): add theme

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,9 @@
-import { Text, View } from 'react-native';
-
-import { styles } from './styles';
+import { Container, Title } from './styles';
 
 export function Groups() {
   return (
-    <View style={styles.container}>
-      <Text>Groups</Text>
-    </View>
+    <Container>
+      <Title>Groups</Title>
+    </Container>
   );
 }
