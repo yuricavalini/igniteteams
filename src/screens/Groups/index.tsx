16c726839cd1119073ed16ac8baa61d91e3df7db
Yuri Cavalini <yuricavalini@gmail.com>
2023-04-13 20:32:15 -0300

    feat(igniteteams): add styled-components example to Groups screens

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,5 @@
-import { Container, Title } from './styles';
+import { Container } from './styles';
 
 export function Groups() {
-  return (
-    <Container>
-      <Title>Groups</Title>
-    </Container>
-  );
+  return <Container />;
 }
