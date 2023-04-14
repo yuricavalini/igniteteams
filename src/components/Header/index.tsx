91a8ffc2e54cff5a1537793b7101d8f8969e3d4a
Yuri Cavalini <yuricavalini@gmail.com>
2023-04-13 21:18:35 -0300

    feat(igniteteams): add BackButton to Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,19 @@
-import { Container, Logo } from './styles';
+import { BackButton, BackIcon, Container, Logo } from './styles';
 
 import logoImg from '@assets/logo.png';
 
-export function Header() {
+type Props = {
+  showBackButton?: boolean;
+};
+
+export function Header({ showBackButton }: Props) {
   return (
     <Container>
+      {showBackButton ? (
+        <BackButton>
+          <BackIcon />
+        </BackButton>
+      ) : null}
       {/* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment */}
       <Logo source={logoImg} />
     </Container>
