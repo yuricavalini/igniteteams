dba2f0649c80ab1ca2f549fb7431e50a3173f681
Yuri Cavalini <yuricavalini@gmail.com>
2023-04-16 17:20:51 -0300

    feat(igniteteams): add FlatList to groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,16 +1,29 @@
+import { useState } from 'react';
+
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
+
+import { FlatList } from 'react-native';
 import { Container } from './styles';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([
+    'Galera da Rocketseat',
+    'Amigos',
+  ]);
+
   return (
     <Container>
       <Header />
 
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
 
-      <GroupCard title="Galera do Ignite" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </Container>
   );
 }
