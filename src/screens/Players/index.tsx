16a26c6d567214d1e4bbac9b87e53db984375706
Yuri Cavalini <yuricavalini@gmail.com>
2023-04-16 19:28:37 -0300

    feat(igniteteams): add Filter component

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,4 +1,5 @@
 import { ButtonIcon } from '@components/ButtonIcon';
+import { Filter } from '@components/Filter';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { Input } from '@components/Input';
@@ -19,6 +20,8 @@ export function Players() {
 
         <ButtonIcon icon="add" />
       </Form>
+
+      <Filter title="Time A" isActive />
     </Container>
   );
 }
