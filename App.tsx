1cc9c8a484254782a5248c8def30902e20394773
Yuri Cavalini <yuricavalini@gmail.com>
2023-04-16 18:49:50 -0300

    feat(igniteteams): refator styles to use css helper function from styled-components

--- App.tsx
+++ App.tsx
@@ -9,7 +9,7 @@ import { ThemeProvider } from 'styled-components/native';
 import theme from './src/theme';
 
 import { Loading } from '@components/Loading';
-import { NewGroup } from '@screens/NewGroup';
+import { Groups } from '@screens/Groups';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -21,7 +21,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <NewGroup /> : <Loading />}
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
