ee9760da058233f8454926e502e96ec7a1b523dd
Yuri Cavalini <yuricavalini@gmail.com>
2023-04-16 18:23:37 -0300

    feat(igniteteams): add NewGroup screen

--- App.tsx
+++ App.tsx
@@ -9,7 +9,7 @@ import { ThemeProvider } from 'styled-components/native';
 import theme from './src/theme';
 
 import { Loading } from '@components/Loading';
-import { Groups } from '@screens/Groups';
+import { NewGroup } from '@screens/NewGroup';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -21,7 +21,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Groups /> : <Loading />}
+      {fontsLoaded ? <NewGroup /> : <Loading />}
     </ThemeProvider>
   );
 }
