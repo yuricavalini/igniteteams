d2d3b30b0671ac584941dd2ec009386f0f883f1c
Yuri Cavalini <yuricavalini@gmail.com>
2023-04-16 19:39:19 -0300

    feat(igniteteams): add list of filters

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -3,9 +3,15 @@ import { Filter } from '@components/Filter';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { Input } from '@components/Input';
-import { Container, Form } from './styles';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
+import { Container, Form, HeaderList, NumberOfPlayers } from './styles';
 
 export function Players() {
+  const [team, setTeam] = useState<string>('Time A');
+  const [players, setPlayers] = useState<string[]>([]);
+
   return (
     <Container>
       <Header showBackButton />
@@ -21,7 +27,22 @@ export function Players() {
         <ButtonIcon icon="add" />
       </Form>
 
-      <Filter title="Time A" isActive />
+      <HeaderList>
+        <FlatList
+          data={['Time A', 'Time B']}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+
+        <NumberOfPlayers>{players.length}</NumberOfPlayers>
+      </HeaderList>
     </Container>
   );
 }
