f17ffda3ecafe5dc9f632fc5ae484ac73f48aeb5
Yuri Cavalini <yuricavalini@gmail.com>
2023-04-16 19:59:53 -0300

    feat(igniteteams): add ListEmpty component into PlayerCard FlatList

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,8 +1,10 @@
+import { Button } from '@components/Button';
 import { ButtonIcon } from '@components/ButtonIcon';
 import { Filter } from '@components/Filter';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { Input } from '@components/Input';
+import { ListEmpty } from '@components/ListEmpty';
 import { PlayerCard } from '@components/PlayerCard';
 import { useState } from 'react';
 import { FlatList } from 'react-native';
@@ -11,7 +13,7 @@ import { Container, Form, HeaderList, NumberOfPlayers } from './styles';
 
 export function Players() {
   const [team, setTeam] = useState<string>('Time A');
-  const [players, setPlayers] = useState<string[]>(['Yuri 1', 'Yuri 2']);
+  const [players, setPlayers] = useState<string[]>([]);
 
   return (
     <Container>
@@ -51,7 +53,17 @@ export function Players() {
         renderItem={({ item }) => (
           <PlayerCard name={item} onRemove={() => {}} />
         )}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Não há pessoas nesse time." />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[
+          { paddingBottom: 100 },
+          players.length === 0 && { flex: 1 },
+        ]}
       />
+
+      <Button title="Remover Turma" type="SECONDARY" />
     </Container>
   );
 }
