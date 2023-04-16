5de393d9776cf1049d71f475503bd363099a9701
Yuri Cavalini <yuricavalini@gmail.com>
2023-04-16 19:52:22 -0300

    feat(igniteteams): add PlayerCard component

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -3,6 +3,7 @@ import { Filter } from '@components/Filter';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { Input } from '@components/Input';
+import { PlayerCard } from '@components/PlayerCard';
 import { useState } from 'react';
 import { FlatList } from 'react-native';
 
@@ -10,7 +11,7 @@ import { Container, Form, HeaderList, NumberOfPlayers } from './styles';
 
 export function Players() {
   const [team, setTeam] = useState<string>('Time A');
-  const [players, setPlayers] = useState<string[]>([]);
+  const [players, setPlayers] = useState<string[]>(['Yuri 1', 'Yuri 2']);
 
   return (
     <Container>
@@ -43,6 +44,14 @@ export function Players() {
 
         <NumberOfPlayers>{players.length}</NumberOfPlayers>
       </HeaderList>
+
+      <FlatList
+        data={players}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <PlayerCard name={item} onRemove={() => {}} />
+        )}
+      />
     </Container>
   );
 }
