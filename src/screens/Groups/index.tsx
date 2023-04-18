2da88998f510a22d63be61badbde2d737852e0f8
Yuri Cavalini <yuricavalini@gmail.com>
2023-04-18 19:44:57 -0300

    feat(igniteteams): add Players screen navigtion

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -25,6 +25,10 @@ export function Groups() {
     }
   }
 
+  function handleOpenGroup(group: string) {
+    navigation.navigate('players', { group });
+  }
+
   useFocusEffect(
     useCallback(() => {
       fetchGroups().catch((error) => console.log(error));
@@ -40,7 +44,9 @@ export function Groups() {
       <FlatList
         data={groups}
         keyExtractor={(item) => item}
-        renderItem={({ item }) => <GroupCard title={item} />}
+        renderItem={({ item }) => (
+          <GroupCard title={item} onPress={() => handleOpenGroup(item)} />
+        )}
         contentContainerStyle={groups.length === 0 && { flex: 1 }}
         ListEmptyComponent={() => (
           <ListEmpty message="Que tal cadastrar a primeira turma?" />
