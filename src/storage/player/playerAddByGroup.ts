a18646036e4614bf07ec226bfe17a882c11ae766
Yuri Cavalini <yuricavalini@gmail.com>
2023-04-20 08:26:56 -0300

    feat(igniteteams): add storage configuration for player collection

---
+++ src/storage/player/playerAddByGroup.ts
@@ -0,0 +1,33 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { PLAYER_COLLECTION } from '@storage/storageConfig';
+import { AppError } from '@utils/AppError';
+
+import { playersGetByGroup } from './playersGetByGroup';
+import { PlayerStorageDTO } from './PlayerStorageDTO';
+
+export async function playerAddByGroup(
+  newPlayer: PlayerStorageDTO,
+  group: string
+) {
+  try {
+    const storedPlayers = await playersGetByGroup(group);
+
+    const playerAlreadyExists = storedPlayers.find(
+      (player) => player.name === newPlayer.name
+    );
+
+    if (!playerAlreadyExists) {
+      throw new AppError('Essa pessoa já está adicionada em um time aqui.');
+    }
+
+    const storage = JSON.stringify([...storedPlayers, newPlayer]);
+
+    /*
+      Example: @ignite-teams:player-rocket: []
+     */
+    await AsyncStorage.setItem(`${PLAYER_COLLECTION}-${group}`, storage);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
---
+++ src/storage/player/playersGetByGroup.ts
@@ -0,0 +1,21 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { PLAYER_COLLECTION } from '@storage/storageConfig';
+
+import { PlayerStorageDTO } from './PlayerStorageDTO';
+
+export async function playersGetByGroup(group: string) {
+  try {
+    const storage: string | null = await AsyncStorage.getItem(
+      `${PLAYER_COLLECTION}-${group}`
+    );
+
+    const players: PlayerStorageDTO[] = storage
+      ? (JSON.parse(storage) as PlayerStorageDTO[])
+      : [];
+
+    return players;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
